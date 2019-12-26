8a841d92eb044ba9914b4975ca03b55210685c8f
Muhsina Miti <muhsinatabassummiti@gmail.com>
2019-12-26 22:55:51 +0600

    routes

--- application/config/routes.php
+++ application/config/routes.php
@@ -49,6 +49,10 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | Examples:	my-controller/index	-> my_controller/index
 |		my-controller/my-method	-> my_controller/my_method
 */
+$route['admin-login'] = 'admin/check_admin_login';
+$route['admin'] = 'admin';
+$route['home-page'] = 'welcome';
 $route['default_controller'] = 'welcome';
+$route['abcd'] = 'admin/index';
 $route['404_override'] = '';
 $route['translate_uri_dashes'] = FALSE;
